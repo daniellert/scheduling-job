346ca04509aa3755eedd28600057eb4e66b3c8aa
Danielle Formagio <danielletonhai@gmail.com>
2020-08-30 14:59:52 -0300

    Scheduling Jobs logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import JobsService from './services/JobsService';
 import './App.css';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    JobsService.scheduling()
+      .then((jobs) => setJobs(jobs));
+  }, []);
 
   return (
     <div className="App">
--- src/services/JobsService.js
+++ src/services/JobsService.js
@@ -1,9 +1,49 @@
-import api from '.api';
+import api from './api';
+import moment from 'moment';
+import { orderBy, sumBy } from 'lodash';
+
+const executionStartDate = moment('2019-11-10 09:00:00');
+const executionEndDate = moment('2019-11-11 12:00:00');
 
 async function getAll() {
-    return await api.getAll('/jobs');
+    const response = await api.get('/jobs');
+    return response.data;
+}
+
+async function scheduling() {
+    const jobs = await getAll();
+    const jobsWithinExecutionWindow = jobs.filter((job) =>
+        moment(job.maxExecutionDate).isSameOrAfter(executionStartDate) &&
+        moment(job.maxExecutionDate).isSameOrBefore(executionEndDate));
+    const filterJobsWithinEstimatedTime = jobsWithinExecutionWindow.filter((job) => job.estimatedTime <= 8);
+    const orderJobsByMaxExecutionDate = orderBy(filterJobsWithinEstimatedTime, (job) => job.maxExecutionDate);
+    const jobsExecutionList = [];
+    const currentDate = executionStartDate;
+
+    orderJobsByMaxExecutionDate.forEach(job => {
+        const estimatedEndTime = moment(currentDate).add(job.estimatedTime, 'hours');
+        if (estimatedEndTime.isSameOrBefore(job.maxExecutionDate)) {
+            if (jobsExecutionList.length === 0) {
+                jobsExecutionList.push([job]);
+            } else {
+                const lastArrayItem = jobsExecutionList[jobsExecutionList.length - 1];
+                const totalTimeInArray = sumBy(lastArrayItem, (job) => job.estimatedTime);
+
+                if (job.estimatedTime + totalTimeInArray > 8) {
+                    jobsExecutionList.push([job]);
+                } else {
+                    lastArrayItem.push(job);
+                }
+            }
+
+            currentDate.add(job.estimatedTime, 'hours');
+        }
+    });
+
+    return jobsExecutionList.map((jobList) => jobList.map((job) => job.id));
 }
 
 export default {
-    getAll
+    getAll,
+    scheduling
 };
